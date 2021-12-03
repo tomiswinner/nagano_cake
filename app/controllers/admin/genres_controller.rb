b5cfe3e42b5a2abd43238ab78c30badb115085ff
tomiswinner <mouseofsack911@gmail.com>
2021-12-03 09:14:16 +0000

    2[add] seed and private param in index.html

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,10 +2,14 @@ class Admin::GenresController < ApplicationController
   
   def index
     @genres = Genre.all
+    @genre = Genre.new
     
   end
   
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save!
+    redirect_to admin_genres
   end
   
   def edit
@@ -13,5 +17,11 @@ class Admin::GenresController < ApplicationController
   
   def update
   end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
   
 end
