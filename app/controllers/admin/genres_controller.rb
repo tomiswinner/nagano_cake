ba4ff15b7254cfe6bfff8b5c4fe8ca3474d90b0c
tomiswinner <mouseofsack911@gmail.com>
2021-12-03 08:21:28 +0000

    [add] add genres controller to app with namespace admin

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,17 @@
+class Admin::GenresController < ApplicationController
+  
+  def index
+    @genres = Genre.all
+    
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+end
