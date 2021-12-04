fdee25a54fb0dc3116205f0741d9338561867add
tomiswinner <mouseofsack911@gmail.com>
2021-12-04 16:47:16 +0000

    [add] add items controller to app\2

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,21 @@
+class Admin::ItemsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+end
