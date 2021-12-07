d16fb622b6479c8fa8c06c0a7df051af17b4f5fa
tomiswinner <mouseofsack911@gmail.com>
2021-12-07 13:06:00 +0000

    [add] show.html to customer

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   
   
   namespace :admin do
+    root :to => "homes#top", as: "home"
     resources :genres, only: [:index,:create,:edit,:update]
     resources :items, only: [:index,:new,:create,:show,:edit,:update]
     resources :customers, only: [:index,:show,:edit,:update]
+    # resources :orders, only: [:show,:update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
