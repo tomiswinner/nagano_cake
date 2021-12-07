5797b65176dad771f26216bdd2e513770e7b23a0
tomiswinner <mouseofsack911@gmail.com>
2021-12-07 09:13:46 +0000

    [add] add index.html and controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:index,:create,:edit,:update]
     resources :items, only: [:index,:new,:create,:show,:edit,:update]
+    resources :customers, only: [:index,:show,:edit,:update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,18 @@
       random_genre_id = rand(Genre.first.id..Genre.last.id)
       Item.create(name:"item#{n}",genre_id: random_genre_id,image_id:"aa",introduction:"#{n}",price: 100 * n, is_active: true)
     end
+
+    10.times do |n|
+      customer = Customer.new
+      customer.email = "hogehoge#{n}@example.com"
+      customer.password = "hogehoge#{n}"
+      customer.last_name = "山田#{n}"
+      customer.first_name  = "太郎#{n}"
+      customer.last_name_kana = "ヤマダ#{n}"
+      customer.first_name_kana = "タロウ#{n}"
+      customer.postal_code = "#{n}12-3456"
+      customer.address = "#{n}city"
+      customer.telephone_number = "#{n}90-0000-0000"
+      customer.save
+      
+    end
