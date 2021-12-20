af8f1297a5a2ba46e82c0a363e516fe450beafc8
tomiswinner <mouseofsack911@gmail.com>
2021-12-20 14:50:44 +0000

    [add] add edit.html

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,6 +7,7 @@ class AddressesController < ApplicationController
   end
   
   def edit
+    @address = Address.find(params[:id])
   end
   
   def create
@@ -28,9 +29,36 @@ class AddressesController < ApplicationController
   end
   
   def update
+    @address = Address.find(params[:id])
+    if @address.update(addresses_params)
+      flash[:notice] = "Address was successfully updated"
+      redirect_to addresses_path
+    else
+      err_msg = "Error! Failed to update\n"
+      @address.errors.full_messages.each do |msg|
+        err_msg += msg + "\n"
+      end
+      
+      flash[:alert] = err_msg
+      render :edit
+    end
   end
   
   def destroy
+    address = Address.find(params[:id])
+    if address.destroy
+      flash[:notice] = "Address was successfully deleted"
+      redirect_to addresses_path
+    else
+      err_msg = "Error! Failed to delete\n"
+      @address.errors.full_messages.each do |msg|
+        err_msg += msg + "\n"
+      end
+      
+      flash[:alert] = err_msg
+      redirect_to addresses_path
+    end
+    
   end
   
   private
