b47ec00a42dcb05a8823a751256b6b206537732e
tomiswinner <mouseofsack911@gmail.com>
2022-01-02 04:48:31 +0000

    [fix] adminroute path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins, path: "admin"
   
   root :to => "homes#top"
   get "about", to: "homes#about", as: "about"
@@ -38,17 +36,17 @@ Rails.application.routes.draw do
   end
   
   
-  # devise_for :customers, controllers: {
-  #   sessions:        'customers/sessions',
-  #   passwords:       'customers/passwords',
-  #   registrations:   'customers/registrations',
-  # }
+  devise_for :customers, controllers: {
+    sessions:        'customers/sessions',
+    passwords:       'customers/passwords',
+    registrations:   'customers/registrations',
+  }
   
-  # devise_for :admins, controllers: {
-  #   sessions:        'admins/sessions',
-  #   passwords:       'admins/passwords',
-  #   registrations:   'admins/registrations',
-  # }
+  devise_for :admins, path: "admin",controllers: {
+    sessions:        'admins/sessions',
+    passwords:       'admins/passwords',
+    registrations:   'admins/registrations',
+  }
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
