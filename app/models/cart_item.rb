b44f2880f2c9920ac09a67b5d1160540eb38aec7
tomiswinner <mouseofsack911@gmail.com>
2022-01-02 13:49:45 +0000

    [fix] set validate to cart_items model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,10 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item    
   
+  validates :customer_id       , presence: true
+  validates :item_id           , presence: true
+  validates :amount          , presence: true
+  
   def subtotal
     item.taxed_price * amount
   end
