30a0b1e41d791c4450056adf829240a7983dafb8
tomiswinner <mouseofsack911@gmail.com>
2022-01-02 05:57:34 +0000

    [add] add serach engine 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    
     if params[:genre_id]
       @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
-      @genres = Genre.all
       @title = Genre.find(params[:genre_id]).name
+      
+    elsif params[:searchword]
+      @items = Item.where("name LIKE ?", "%#{params[:searchword]}%").page(params[:page]).per(8)
+      @title = "\"" + params[:searchword] + "\" での検索結果"
+      
     else
       @items = Item.page(params[:page]).per(8)
-      @genres = Genre.all
       @title = "商品"
+      
     end
   end
   
