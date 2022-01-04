8bba4976e50457b028678d4eff11e4485ec92e55
tomiswinner <mouseofsack911@gmail.com>
2022-01-04 01:16:49 +0000

    [fix] items index count

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,14 +6,17 @@ class ItemsController < ApplicationController
     @genres = Genre.all
     
     if params[:genre_id]
+      @count = Item.where(genre_id: params[:genre_id]).count
       @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
       @title = Genre.find(params[:genre_id]).name
       
     elsif params[:searchword]
+      @count = Item.where("name LIKE ?", "%#{params[:searchword]}%").count
       @items = Item.where("name LIKE ?", "%#{params[:searchword]}%").page(params[:page]).per(8)
       @title = "\"" + params[:searchword] + "\" での検索結果"
       
     else
+      @count = Item.count
       @items = Item.page(params[:page]).per(8)
       @title = "商品"
       
